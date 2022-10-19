53450bf14df6cb41968275a73330e2f2600d65d5
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-10-19 16:26:52 +0200

    refact: sending response files

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const path = require("path")
 
 const app = express();
 
+const sendResponse = (response, fileToSend) => {
+    response.sendFile(path.join(__dirname, 'views', fileToSend + '.html'));
+};
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'accueil.html'));
+    sendResponse(res, 'accueil');
 });
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+    sendResponse(res, 'about');
 });
 app.get('/portfolio', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'portfolio.html'));
+    sendResponse(res, 'portfolio');
 });
 
 app.listen(process.env.PORT || 8080);
